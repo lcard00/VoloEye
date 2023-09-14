7b9149aa4092285c75da0f8d824ded406a458a27
Leonardo Cardoso <leocsilvaa@gmail.com>
2023-09-13 22:40:26 -0300

    conveert variables tostring()

--- src/main.lua
+++ src/main.lua
@@ -64,14 +64,14 @@ Ext.Events.GameStateChanged:Subscribe(function(e)
 
             local item_name, item_template = utils.get_item_data(item)
             if debug then
-                utils.print_msg("item_name........: " .. item_name, 2, 2)
-                utils.print_msg("item_template....: " .. item_template, 2, 2)
+                utils.print_msg("item_name........: " .. tostring(item_name), 2, 2)
+                utils.print_msg("item_template....: " .. tostring(item_template), 2, 2)
             end
 
             local item_found, change_eye_color = utils.read_item(item_template, 1, false)
             if debug then
-                utils.print_msg("item_found......: " .. item_found, 2, 2)
-                utils.print_msg("change_eye_color: " .. change_eye_color, 2, 2)
+                utils.print_msg("item_found......: " .. tostring(item_found), 2, 2)
+                utils.print_msg("change_eye_color: " .. tostring(change_eye_color), 2, 2)
             end
             if change_eye_color and item_found and mod_uuid == "ebc37348-b345-4d49-a28f-c0491824d755" then
                 utils.remove_material_ovveride(character, visual_type, debug)
